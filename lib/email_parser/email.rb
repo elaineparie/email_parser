42149d59d4861523b0bde0878a4dcf673f63b376
github username <github email address>
2018-12-24 08:07:03 -0500

    formats email print

--- lib/email_parser/email.rb
+++ lib/email_parser/email.rb
@@ -1,19 +1,19 @@
 class EmailParser::Email
 
-    attr_accessor :subject, :to, :from, :message_id
+  attr_accessor :subject, :to, :from, :message_id
 
 
   def self.create_emails(emails)
     @emails = []
     emails.each do |email|
-      email = slice_quotes(email)
+    email = slice_quotes(email)
     new_email = self.new
     sort_by_subject(email, new_email)
     sort_by_message_id(email, new_email)
     sort_by_from(email, new_email)
     sort_by_to(email, new_email)
         @emails << new_email
-  end
+    end
     @emails
   end
 
@@ -22,13 +22,14 @@ class EmailParser::Email
   end
 
   def self.sort_by_subject(email, new_email)
-    # binding.pry
       new_email.subject = email.split("Subject:")[1].split("Content")[0].strip.downcase
     if new_email.subject.include?("re:")
       new_email.subject = new_email.subject.split("re:")[1].strip
     end
+    if new_email.subject.include?("fw:")
+      new_email.subject = new_email.subject.split("fw:")[1].strip
+    end
   end
-
 
   def  self.sort_by_message_id(email, email_object)
       email_object.message_id = email.split("Message-ID:")[1].split("Subject:")[0].strip.downcase
@@ -43,36 +44,46 @@ class EmailParser::Email
   end
 
   def self.group_into_convos(emails)
-    # binding.pry
     email_hash = {}
     emails = self.create_emails(emails)
     @emails = emails.group_by(&:subject)
-    # print_emails
     email_hash["promotion"] = {}
     email_hash["personal"] = {}
     @emails.each do |email|
       email_h = {email[0] => email[1]}
-      # binding.pry
-      if email.flatten[1].from.include?("promotion")
+        if email.flatten[1].from.include?("promotion")
            email_hash["promotion"].merge!(email_h)
-           # binding.pry
         else
           email_hash["personal"].merge!(email_h)
-
         end
-      end
-      print_emails(email_hash)
     end
+      print_emails(email_hash)
+  end
 
-  def self.print_emails(email_hash)
-    # binding.pry
-      puts "Enter 'Personal' to view your personal inbox or 'Promotion' to view your promotional inbox"
+    def self.print_emails(email_hash)
+      puts "
+      Enter 'Personal' to view your personal inbox or 'Promotion' to view your promotional inbox
+      "
       input = gets.strip.downcase
       if input == "personal"
-        puts email_hash["personal"]
+        email_hash["personal"].each do |key, value|
+          puts "
+          Subject: #{key}:
+          "
+            value.each do |email|
+              puts email
+            end
+          end
       elsif input == "promotion"
-         puts email_hash["promotion"]
+        email_hash["promotion"].each do |key, value|
+          puts "
+          Subject: #{key}:
+          "
+          value.each do |email|
+            puts email
+          end
       end
+    end
   end
 
 
--- lib/email_parser/importer.rb
+++ lib/email_parser/importer.rb
@@ -1,92 +1,15 @@
-require 'pry'
- # require_relative '../../db/raw_emails/2017-11-02-18:58:15.mail'
+
 class EmailParser::Importer
 
-
-#   attr_accessor :path
-#
-#
-#
-# def initialize(path)
-#   @path = path
-# end
-
-
-def self.import_files
-  files = []
-  some_dir = Dir.open "./db/raw_emails"
-  # some_dir.delete(".")
-  some_dir.each do |file|
-    if file != "." && file != ".." && file != ".DS_Store"
-      file = File.open("db/raw_emails/#{file}").read
-      files << file
-    end
-
-  # file = File.absolute_path file # => "/users/andrew/Documents/plans.txt"
+  def self.import_files
+    files = []
+    some_dir = Dir.open "./db/raw_emails"
+      some_dir.each do |file|
+        if file != "." && file != ".." && file != ".DS_Store"
+          file = File.open("db/raw_emails/#{file}").read
+          files << file
+        end
+      end
+    EmailParser::Email.group_into_convos(files)
   end
-# binding.pry
-    # use it here
-
-    emails = files
-  # emails = files.each do |file|
-  #   file = File.open(file, "r")
-  #   file.each do |line|
-  #     binding.pry
-  #   end
-  # end
-# ["From: Carmella Draeger <carmella@example.com>
-# #   To: Celia Prince <celia@example.com>, Alisson Silva <alisson@example.com>
-# #   Cc: iyana@example.com
-# #   Message-ID: <1@example>
-# #   Subject: Meeting
-# #   Date: 2 Nov 2017 18:58:15 +0000
-# #
-# #   Hi both, can you let me know ideal times for meeting please?",
-#   "From: Celia Prince <celia@example.com>
-#   To: Carmella Draeger <carmella@example.com>
-#   Cc: iyana@example.com, Alisson Silva <alisson@example.com>
-#   Message-ID: <2@example>
-#   In-Reply-To: <1@example>
-#   Subject: RE: Meeting
-#   Date: 2 Nov 2017 20:38:12 +0000
-#
-#   Hey Carmella,
-#
-#   Tuesday would be best for me. I'm otherwise very busy.
-#
-#   Best,
-#   Celia
-#
-#   > Hi both, can you let me know ideal times for meeting please?
-# ",
-# "From: Red Hot Deals!!! <promotions@example.com>
-# To: Carmella Draeger <carmella@example.com>
-# Message-ID: <5@example>
-# Subject: Fidget spinners REDUCED TO CLEAR
-# Date: 4 Nov 2017 11:22:00 +0000
-#
-# Check out this deal - available today only!
-#
-# https://redhot.deals/3f98j23/fidget-spinners-reduced-to-clear",
-# "From: Alisson Silva <alisson@example.com>
-# To: Carmella Draeger <carmella@example.com>
-# Message-ID: <3@example>
-# Subject: Re: Meeting
-# Date: 2 Nov 2017 20:42:55 +0000
-#
-# Wide open, literally any day works!
-# "]
-
-  EmailParser::Email.group_into_convos(emails)
-end
-
-# def import
-#   filename = files
-#   filename.each do |file|
-# Song.new_by_filename(file)
-# end
-# end
-
-
-
 end
